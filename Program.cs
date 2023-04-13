93bf4740e8c1d91bad32978d528d4c1ebf93bd41
Vitor Lassen <vitor@lassen.com.br>
2023-04-13 20:43:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(RetornaNumero(5));
+Func<int, double> RetornaNumFunc = new Func<int, double>(num1 => num1+1.5);
+Console.WriteLine(RetornaNumFunc(5));
+
+
+
+double RetornaNumero(int num1){
+  return num1+1.5;
+}
+
+
+//int RetornaNumero(int num1) => num1+1;
